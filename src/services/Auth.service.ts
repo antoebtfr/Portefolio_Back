5bc116b4620a5c0ca992ba645fa0879d5b592ac0
antoebtfr <antoebt@hotmail.com>
2021-03-12 12:04:31 +0100

    fix

--- src/services/Auth.service.ts
+++ src/services/Auth.service.ts
@@ -107,7 +107,6 @@ export class AuthService {
         const transporter = createTransport({
             host: 'smtp.ethereal.email',
             port: 587,
-            secure: false,
             auth : {
                 user: testAccount.user,
                 pass: testAccount.pass,
