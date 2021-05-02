c5e2078b86424e02912b1819e560853ba55e50ac
antoebtfr <antoebt@hotmail.com>
2021-05-03 00:45:46 +0200

    connection repo

--- src/services/User.service.ts
+++ src/services/User.service.ts
@@ -1,28 +1,28 @@
 import { getCustomRepository, getRepository } from "typeorm";
 import { Connection } from "../entities/Connection";
 import { User } from "../entities/User";
+import { ConnectionRepository } from "../repositories/connection.repository";
 import { UserRepository } from "../repositories/user.repository";
 
 export class UserService {
     protected repo = getCustomRepository(UserRepository);
+    protected logRepo = getCustomRepository(ConnectionRepository);
 
-    get(){
+    get() {
         return this.repo.find();
     }
 
-    post(User: User){
+    post(User: User) {
         console.log('Object has been added');
         return this.repo.save(User);
     }
 
-    logIp(connectionLog: Connection){
-        console.log('testws20')
-        this.repo.save(connectionLog);
-        console.log('good');
-        }
+    logIp(connectionLog: Connection) {
+        return this.logRepo.save(connectionLog);
+    }
 
-    async activeUserAccount(user: User){
-        user.isConfirmed = true; 
+    async activeUserAccount(user: User) {
+        user.isConfirmed = true;
         this.repo.update(user.id, user);
     }
 
